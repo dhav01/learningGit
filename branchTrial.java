6095383e535a9e0d70b6f598fcac41b21eb43769
dhaval <dhagrawal0@gmail.com>
2021-04-16 09:59:21 +0530

    new print statement added

--- branchTrial.java
+++ branchTrial.java
@@ -4,5 +4,6 @@ public class branchTrial {
     public static void main(String[] args) {
 
         System.out.println("creating new branch file and merging it to master branch");
+        System.out.println("making a new change in newBranch");
     }
 }
