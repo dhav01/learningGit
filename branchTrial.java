00003d9e39b2fa22bce0ce9f7caf9570c56947b6
dhaval <dhagrawal0@gmail.com>
2021-04-16 09:57:37 +0530

    new branch file added

---
+++ branchTrial.java
@@ -0,0 +1,8 @@
+package algo;
+
+public class branchTrial {
+    public static void main(String[] args) {
+
+        System.out.println("creating new branch file and merging it to master branch");
+    }
+}
